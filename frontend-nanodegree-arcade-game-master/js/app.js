4d778383acf86407172b6fff3876795e51d8174b
Tarnil <camille.frisquet@gmail.com>
2016-08-02 23:49:37 +0100

    menu added

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -5,15 +5,162 @@ var colWidth = 101;
 var rowHeight = 83;
 var padding = 30;
 var speedMultiplier = 200;
+var enemyNumArray = ["enemy",4,6,8];
+var enemyRowsArray = ["row of enemy",3,4,5];
+var colNumArray = ["column",5,7,9];
+var optionArray = [enemyNumArray, enemyRowsArray, colNumArray];
 var enemyRows = 3;
-var playerRow = enemyRows + 3;
-var colNum = 7;
+var colNum = 5;
 var enemyNum = 4;
+var selectedOption = [enemyNum, enemyRows, colNum];
 var score = 0;
+var charArray = ['images/char-boy.png',
+                'images/char-cat-girl.png',
+                'images/char-horn-girl.png',
+                'images/char-pink-girl.png'];
+var selectedChar = 'images/char-boy.png';
+
+var optionRender = function (array, index) {
+    ctx.textAlign = "left";
+    ctx.fillText("Number of " + array[0] + ":", 0, colWidth * (2 * index + 1) - 50);
+    for (i = 1; i < array.length ; i++) {
+        ctx.fillText('' + array[i], 2 * (i - 1) * colWidth + 50, colWidth * (1 + index * 2) -10);
+        ctx.fillRect(2 * (i - 1) * colWidth, (2 * index + 1) * colWidth, colWidth, colWidth);
+        if (array[i] == selectedOption[index]) {
+            ctx.save();
+            ctx.fillStyle = "red";
+            ctx.fillRect(2 * (i - 1) * colWidth + 10, (2 * index + 1) * colWidth + 10, colWidth - 20, colWidth -20);
+            ctx.restore();
+            }
+    }
+};
 
 
+var Menu = function() {
+    this.isUp = true;
+    this.optionUp = true;
+    this.gameRun = false;
+    this.input = 3;
+    this.inputX = 0;
+    this.inputY = 0;
+};
+Menu.prototype.initGame = function () {
+    player.initPosition();
+    boss.initPosition();
+    blue.initPosition();
+    orange.initPosition();
+    green.initPosition();
+    star.initPosition();
+}
+Menu.prototype.render = function() {
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "white";
+    //Render the option
+    if (this.optionUp) {
+        ctx.clearRect (0, 0, colNum * colWidth, (enemyRows + 5) * colWidth);
+        for (index = 0; index < optionArray.length; index ++) {
+            optionRender(optionArray[index], index);
+        }
+        ctx.fillRect(3 * colWidth, 7 * colWidth, 2 * colWidth, colWidth);
+        ctx.save();
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 8;
+        ctx.textAlign = "center";
+        ctx.fillText("Next", 4 * colWidth , 7 * colWidth + 50);
+        if (this.inputY != 3) {
+            ctx.strokeRect(this.inputX * 2 * colWidth, (2 * this.inputY + 1) * colWidth, colWidth, colWidth);
+        }
+        if (this.inputY == 3) {
+            ctx.strokeRect(3 * colWidth, 7 * colWidth, 2 * colWidth, colWidth);
+        }
+        ctx.restore();
+    }
+    //Render selection of character
+    if (!this.optionUp) {
+        ctx.clearRect (0, 0, colNum * colWidth, (enemyRows + 5) * colWidth);
+        ctx.save();
+        ctx.font = "30px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText("Select your Character", colWidth * 5 / 2, colWidth);
+        ctx.drawImage(Resources.get('images/Selector.png'), colWidth * 2 , colWidth * 2);
+        for (i = 0; i < charArray.length; i++) {
+            ctx.drawImage(Resources.get(charArray[i]), colWidth * (i + this.input - 1), colWidth * 2);
+        }
+        ctx.fillRect(3 * colWidth, 7 * colWidth, 2 * colWidth, colWidth);
+        ctx.fillStyle = "black";
+        ctx.fillText("Next", 4 * colWidth , 7 * colWidth + 50);
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 8;
+        if (this.inputY == 1) {
+            ctx.strokeRect(3 * colWidth, 7 * colWidth, 2 * colWidth, colWidth);
+        }
+        ctx.restore();
 
+    }
+};
+
+Menu.prototype.handleInput = function(key) {
+    if (this.optionUp) {
+        if (key == "left" && this.inputX !== 0) {
+            this.inputX -= 1;
+        }
+        if (key == "right" && this.inputX != 2) {
+            this.inputX += 1;
+        }
+        if (key == "up" && this.inputY !== 0) {
+            this.inputY -= 1;
+        }
+        if (key == "down" && this.inputY != 3) {
+            this.inputY += 1;
+        }
+        if (key == "enter") {
+            if (this.inputY === 0) {
+                enemyNum = enemyNumArray[this.inputX + 1];
+                selectedOption[0] = enemyNum;
+            }
+            if (this.inputY == 1) {
+                enemyRows = enemyRowsArray[this.inputX +1];
+                selectedOption[1] = enemyRows;
+                document.getElementsByTagName("canvas").height = (enemyRows + 5) * colWidth;
+            }
+            if (this.inputY == 2) {
+                colNum = colNumArray[this.inputX + 1];
+                selectedOption[2] = colNum;
+                document.getElementsByTagName("canvas").width = colNum * colWidth;
+            }
+            if (this.inputY == 3) {
+                this.optionUp = false;
+                this.inputY = 0;
+            }
+        }
+    }
+    if (!this.optionUp) {
+        if (key == "left" && this.input != 3) {
+            this.input += 1;
+        }
+        if (key == "right" && this.input !== 0) {
+            this.input -= 1;
+        }
+        if (key == "up" && this.inputY !== 0) {
+            this.inputY -= 1;
+        }
+        if (key == "down" && this.inputY != 1) {
+            this.inputY += 1
+        }
+        if (this.inputY == 1) {
+            if (key == "enter") {
+                selectedChar = charArray[3 - this.input];
+                this.isUp = false;
+                ctx.clearRect (0, 0, colNum * colWidth, (enemyRows + 5) * colWidth);
+                this.initGame();
+            }
+        }
+    }
+};
+var menu = new Menu();
 // Enemies our player must avoid
+
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -52,6 +199,20 @@ Enemy.prototype.update = function(dt) {
             }
         }
     }
+// Add enemy during a game
+    if (!player.bossFight) {
+        if (allEnemies.length < enemyNum) {
+            for (i = allEnemies.length; i < enemyNum; i++) {
+                allEnemies.push(new Enemy);
+            }
+        }
+    }
+// Remove enemy during a game
+    if (!player.bossFight) {
+        if (allEnemies.length > enemyNum) {
+            allEnemies.splice(Enemy, (allEnemies.length - enemyNum));
+        }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -60,10 +221,13 @@ Enemy.prototype.render = function() {
 };
 // When all gems are gathered the boss appear and shoot one star toward the player
 var EnemyBoss = function() {
-    this.positionX = Math.floor(colNum / 2) * colWidth;
+    this.initPosition();
     this.positionY = rowHeight - padding;
     this.speed = speedMultiplier;
     this.sprite = 'images/char-princess-girl.png'
+};
+EnemyBoss.prototype.initPosition = function() {
+    this.positionX = Math.floor(colNum / 2) * colWidth;
 };
 
 EnemyBoss.prototype.update = function(dt) {
@@ -78,11 +242,10 @@ EnemyBoss.prototype.update = function(dt) {
 EnemyBoss.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
 };
-var boss = new EnemyBoss();
 // Projectile used by the boss
 var EnemyStar = function() {
     this.initPosition();
-    this.speed = 400;
+    this.speed = speedMultiplier;
     this.sprite = 'images/Star.png';
 };
 EnemyStar.prototype.initPosition = function() {
@@ -90,10 +253,10 @@ EnemyStar.prototype.initPosition = function() {
     this.positionY = rowHeight - padding;
 };
 EnemyStar.prototype.update = function(dt) {
-    if (this.positionY < playerRow * rowHeight - padding) {
+    if (this.positionY < (enemyRows + 3) * rowHeight - padding) {
         this.positionY += this.speed * dt;
     }
-    if (this.positionY >= playerRow * rowHeight - padding) {
+    if (this.positionY >= (enemyRows + 3) * rowHeight - padding) {
         this.initPosition();
     }
     if (this.positionX <= player.positionX + 50 && this.positionX >= player.positionX - 50) {
@@ -107,7 +270,6 @@ EnemyStar.prototype.update = function(dt) {
 EnemyStar.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
 };
-var star = new EnemyStar()
 // Gem to grab to win
 var Gem = function(color) {
     this.positionY = rowHeight - padding;
@@ -144,7 +306,7 @@ Gem.prototype.initPosition = function(color) {
 };
 
 Gem.prototype.update = function() {
-//
+//To avoid overlapping of gems
     if (gemArray != []) {
         for (var i = 0; i < gemArray.length; i++) {
             if (i != gemArray.indexOf(this)){
@@ -167,10 +329,10 @@ var Player = function() {
     this.initPosition();
     this.initLife();
     this.bossFight = false;
-    this.sprite = 'images/char-boy.png';
+    this.sprite = selectedChar;
     this.heartSprite = 'images/Heart.png'
 };
-
+// Initialise gems
 Player.prototype.initGems = function() {
     gemArray = [];
     blue.initPosition();
@@ -181,7 +343,7 @@ Player.prototype.initGems = function() {
     gemArray.push(green);
     this.gemCount = 0;
 };
-
+// Initialise life and gems, and check if boss is up to reinitialise it
 Player.prototype.initLife = function() {
     this.life = 3;
     this.initGems();
@@ -194,9 +356,9 @@ Player.prototype.initLife = function() {
 };
 
 Player.prototype.initPosition = function() {
-    //Start on column of the middle and last row
+    //Start on column of the middle and before last row
     this.positionX = Math.floor(colNum / 2) * colWidth;
-    this.positionY = playerRow * rowHeight - padding;
+    this.positionY = (enemyRows + 3) * rowHeight - padding;
 };
 
 Player.prototype.update = function() {
@@ -241,7 +403,7 @@ Player.prototype.update = function() {
     }
 // enemy collision
     for (var i = 0; i < allEnemies.length; i++) {
-        if (allEnemies[i].positionY == this.positionY) {
+        if (allEnemies[i].positionY == this.positionY && allEnemies[i] != boss) {
             if (this.positionX <= allEnemies[i].positionX + 50 && this.positionX >= allEnemies[i].positionX - 50) {
                 this.initPosition();
                 this.life -= 1;
@@ -262,21 +424,21 @@ Player.prototype.update = function() {
         score = 0;
     }
 };
-//Draw the player in the game and the number of life
+//Draw the player in the game, the number of life and score
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
+    ctx.drawImage(Resources.get(selectedChar), this.positionX, this.positionY);
     for (i = 1; i <= this.life; i++) {
-        ctx.drawImage(Resources.get(this.heartSprite), (colNum - i) * colWidth, (playerRow + 1) * rowHeight);
+        ctx.drawImage(Resources.get(this.heartSprite), (colNum - i) * colWidth, (enemyRows + 4) * rowHeight);
     }
     ctx.font = "50px Arial"
-    ctx.fillText("" + score, 0, (playerRow +1) * rowHeight + 101);
+    ctx.fillText("" + score, 0, (enemyRows + 4) * rowHeight + 101);
 };
 //Handle the input of the keyboard to move the player
 Player.prototype.handleInput = function(key) {
     if (key == 'up') {
         this.positionY -= rowHeight;
     }
-    if (key == 'down' && this.positionY != playerRow * rowHeight - padding) {
+    if (key == 'down' && this.positionY != (enemyRows + 3) * rowHeight - padding) {
         this.positionY += rowHeight;
     }
     if (key == 'left' && this.positionX !== 0) {
@@ -289,6 +451,7 @@ Player.prototype.handleInput = function(key) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
 var blue = new Gem("blue");
 var orange = new Gem("orange");
 var green = new Gem("green");
@@ -298,7 +461,8 @@ var allEnemies = new Array();
 for (i = 1; i <= enemyNum; i++) {
     allEnemies.push(new Enemy());
 }
-
+var boss = new EnemyBoss();
+var star = new EnemyStar();
 
 
 
@@ -309,8 +473,13 @@ document.addEventListener('keyup', function(e) {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
+        40: 'down',
+        13: 'enter'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    if (!menu.isUp) {
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
+    if (menu.isUp) {
+        menu.handleInput(allowedKeys[e.keyCode]);
+    }
 });
--- frontend-nanodegree-arcade-game-master/js/engine.js
+++ frontend-nanodegree-arcade-game-master/js/engine.js
@@ -20,15 +20,15 @@ var Engine = (function(global) {
      * set the canvas elements height/width and add it to the DOM.
      */
     var doc = global.document,
-        win = global.window,
-        canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        lastTime;
-
-    canvas.width = colNum * 101;
-    //changed the canvas height to include functionality
-    canvas.height = (enemyRows + 5) * 101;
+    win = global.window,
+    canvas = doc.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    lastTime;
+    canvas.width = colNum * colWidth;
+    //changed the canvas height to include option
+    canvas.height = (enemyRows + 5) * colWidth;
     doc.body.appendChild(canvas);
+
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -46,9 +46,18 @@ var Engine = (function(global) {
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        update(dt);
+         if (!menu.isUp && !menu.gameRun) {
+              doc.body.removeChild(canvas)
+              canvas.width = colNum * colWidth;
+              //changed the canvas height to include option
+              canvas.height = (enemyRows + 5) * colWidth;
+              doc.body.appendChild(canvas);
+              menu.gameRun = true;
+         }
+        if (menu.gameRun) {
+            update(dt);
+        }
         render();
-
         /* Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
          */
@@ -57,6 +66,7 @@ var Engine = (function(global) {
         /* Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
+
         win.requestAnimationFrame(main);
     }
 
@@ -111,39 +121,44 @@ var Engine = (function(global) {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
-        var rowImages = [];
-        rowImages.push('images/water-block.png');   // Top row is water to avoid graphical glitch
-        rowImages.push('images/water-block.png');   // Second row is water
-        for (i = 1; i <= enemyRows; i++) {
-            rowImages.push('images/stone-block.png');   // Row of stone
-        }
-
-        rowImages.push('images/grass-block.png');   // Row 1 of 2 of grass
-        rowImages.push('images/grass-block.png');   // Row 2 of 2 of grass
-        rowImages.push('images/stone-block.png');    // Row for life and score
-
-        var numRows = rowImages.length,
-            numCols = colNum,
-            row, col;
-
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+        if (menu.gameRun) {
+            var rowImages = [];
+            rowImages.push('images/water-block.png');   // Top row is water to avoid graphical glitch
+            rowImages.push('images/water-block.png');   // Second row is water
+            for (i = 1; i <= enemyRows; i++) {
+                rowImages.push('images/stone-block.png');   // Row of stone
             }
-        }
 
-        renderEntities();
+            rowImages.push('images/grass-block.png');   // Row 1 of 2 of grass
+            rowImages.push('images/grass-block.png');   // Row 2 of 2 of grass
+            rowImages.push('images/stone-block.png');    // Row for life and score
+
+            var numRows = rowImages.length,
+                numCols = colNum,
+                row, col;
+
+            /* Loop through the number of rows and columns we've defined above
+             * and, using the rowImages array, draw the correct image for that
+             * portion of the "grid"
+             */
+            for (row = 0; row < numRows; row++) {
+                for (col = 0; col < numCols; col++) {
+                    /* The drawImage function of the canvas' context element
+                     * requires 3 parameters: the image to draw, the x coordinate
+                     * to start drawing and the y coordinate to start drawing.
+                     * We're using our Resources helpers to refer to our images
+                     * so that we get the benefits of caching these images, since
+                     * we're using them over and over.
+                     */
+                    ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                }
+            }
+
+            renderEntities();
+        }
+        if (menu.isUp) {
+            menu.render();
+        }
     }
 
     /* This function is called by the render function and is called on each game
@@ -186,7 +201,11 @@ var Engine = (function(global) {
         'images/Star.png',
         'images/Gem Blue.png',
         'images/Gem Orange.png',
-        'images/Gem Green.png'
+        'images/Gem Green.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/Selector.png'
     ]);
     Resources.onReady(init);
 
